565fc904aadc289e1f236dc186827782b2ead072
keepforever <briancilenti@gmail.com>
2019-02-28 11:44:49 -0800

    added allDecks query for CommunityDecks to feed Decks to CommunityDecks front-end component

--- src/resolvers/q/index.js
+++ src/resolvers/q/index.js
@@ -1,8 +1,13 @@
 const { me } = require("./me");
+const { allDecks } = require("./allDecks");
+
 
 const q = {
   async me(parent, args, ctx) {
     return await me(parent, args, ctx);
+  },
+  async allDecks(parent, args, ctx) {
+    return await allDecks(parent, args, ctx);
   }
 };
 
