c27291699f9cd57937696c96489b4dc4e1bbb8f1
keepforever <briancilenti@gmail.com>
2019-03-11 13:14:49 -0700

    added singleUser query to support view-user-profile route on client

--- src/resolvers/q/index.js
+++ src/resolvers/q/index.js
@@ -1,6 +1,7 @@
 const { me } = require("./me");
 const { allDecks } = require("./allDecks");
 const { singleDeck } = require("./singleDeck");
+const { singleUser } = require("./singleUser");
 
 const q = {
   async me(parent, args, ctx) {
@@ -11,6 +12,9 @@ const q = {
   },
   async singleDeck(parent, args, ctx) {
     return await singleDeck(parent, args, ctx);
+  },
+  async singleUser(parent, args, ctx) {
+    return await singleUser(parent, args, ctx);
   },
 
 };
